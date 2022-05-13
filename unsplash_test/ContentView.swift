6501ccfbeced3781deda26752f0ea8bae728a323
George <gtevosov@gmail.com>
2022-05-13 19:55:41 +0300

    Initial Commit

---
+++ unsplash_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  unsplash_test
+//
+//  Created by George Tevosov on 13.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ unsplash_test/unsplash_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  unsplash_testApp.swift
+//  unsplash_test
+//
+//  Created by George Tevosov on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct unsplash_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
